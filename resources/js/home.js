b23463a92c6eb6e4f797783dc702bd9a1711c48c
Ebbe Wertz <ebbe.wertz8@gmail.com>
2026-09-20 23:59:55 +0200

    home en footer update

--- resources/js/home.js
+++ resources/js/home.js
@@ -11,7 +11,7 @@ window.addEventListener('load', () => {
     const ctaContainer = document.querySelector('#cta-container');
     if (ctaContainer) ctaContainer.classList.remove('opacity-0');
 
-    // Double Chalk Underline for Motto
+    // Double Chalk Underline voor Motto
     const leuze = document.querySelector('#leuze-text');
     if (leuze) {
       const aLeuze = annotate(leuze, {
@@ -24,7 +24,7 @@ window.addEventListener('load', () => {
       aLeuze.show();
     }
 
-    // Rough annotations for action buttons
+    // Rough annotations voor Hero knoppen
     const btnLid = document.querySelector('#btn-lid');
     if (btnLid) {
       const a1 = annotate(btnLid, {
@@ -49,11 +49,59 @@ window.addEventListener('load', () => {
       a2.show();
     }
 
+    // Rough annotations voor Content Section links
+    const btnOverOns = document.querySelector('#btn-over-ons');
+    if (btnOverOns) {
+      const a3 = annotate(btnOverOns, {
+        type: 'underline',
+        color: '#991b1b',
+        strokeWidth: 1.5,
+        animationDuration: 1000
+      });
+      a3.show();
+    }
+
+    const btnAgenda = document.querySelector('#btn-agenda');
+    if (btnAgenda) {
+      const a4 = annotate(btnAgenda, {
+        type: 'underline',
+        color: '#075985',
+        strokeWidth: 1.5,
+        animationDuration: 1000
+      });
+      a4.show();
+    }
+
+    // Rough annotations (Boxes) voor Social Knoppen
+    const btnInsta = document.querySelector('#btn-insta');
+    if (btnInsta) {
+      const aInsta = annotate(btnInsta, {
+        type: 'box',
+        color: '#db2777',
+        strokeWidth: 1.5,
+        padding: 4,
+        animationDuration: 1000
+      });
+      aInsta.show();
+    }
+
+    const btnFb = document.querySelector('#btn-fb');
+    if (btnFb) {
+      const aFb = annotate(btnFb, {
+        type: 'box',
+        color: '#0284c7',
+        strokeWidth: 1.5,
+        padding: 4,
+        animationDuration: 1000
+      });
+      aFb.show();
+    }
+
     setTimeout(applyChalkFilterToNotation, 50);
   }, 150);
 });
 
-// Global function for popup toggle
+// Globale functie voor de spiekbriefje pop-up
 window.toggleStickyNote = function() {
   const sticky = document.getElementById('sticky-note');
   if (sticky) {
